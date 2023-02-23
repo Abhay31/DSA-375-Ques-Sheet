5dc08163c341c3f4821883e5252ba30962d1bb61
Abhay Sharma <85064231+Abhay31@users.noreply.github.com>
2023-02-24 00:04:15 +0530

    day-1 done

---
+++ 01. Arrays/03. Maximum Subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maximum = INT_MIN;
+        int sum = 0;
+        for(auto it: nums)
+        {
+            sum += it;
+            maximum = max(maximum,sum);
+            if(sum < 0)
+            {
+                sum = 0;
+            }
+        }
+        return maximum;
+    }
+};
+//LEETCODE
